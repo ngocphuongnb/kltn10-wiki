41037b13944306bc16474b0231c5f9a31d9da833
vinhpham.it <vinhpham.it@62b99114-2740-11df-a76c-adfa62dee608>
2010-03-04 03:57:24 +0000

    first commit

---
+++ XLTV/src/xltv/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package xltv;
+
+/**
+ *
+ * @author VinhPham
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s = "H¹NH PHóC LANG THANG NH7 M¢Y, CHO HåN HÐO GÇY";
+        boolean resultTCVN = VNString.checkTCVN(s);
+    }
+
+}
