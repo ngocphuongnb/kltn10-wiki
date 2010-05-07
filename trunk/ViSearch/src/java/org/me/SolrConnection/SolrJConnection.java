6a8bbacae1e844064b4b5b6c58ff0161d62d3914
vinhpham.it <vinhpham.it@62b99114-2740-11df-a76c-adfa62dee608>
2010-05-07 06:43:03 +0000

    first commit

--- trunk/ViSearch/src/java/org/me/SolrConnection/SolrJConnection.java
+++ trunk/ViSearch/src/java/org/me/SolrConnection/SolrJConnection.java
@@ -17,15 +17,15 @@ public class SolrJConnection {
     public static SolrServer getSolrServer() throws MalformedURLException {
         String url = "http://localhost:8983/solr";
         CommonsHttpSolrServer server = new CommonsHttpSolrServer(url);
-        server.setSoTimeout(1000);  // socket read timeout
-        server.setConnectionTimeout(100);
-        server.setDefaultMaxConnectionsPerHost(100);
-        server.setMaxTotalConnections(100);
-        server.setFollowRedirects(false);  // defaults to false
-        // allowCompression defaults to false.
-        // Server side must support gzip or deflate for this to have any effect.
-        server.setAllowCompression(true);
-        server.setMaxRetries(1); // defaults to 0.  > 1 not recommended.
+//        server.setSoTimeout(1000);  // socket read timeout
+//        server.setConnectionTimeout(1000);
+//        server.setDefaultMaxConnectionsPerHost(1000);
+//        server.setMaxTotalConnections(100);
+//        server.setFollowRedirects(false);  // defaults to false
+//        // allowCompression defaults to false.
+//        // Server side must support gzip or deflate for this to have any effect.
+//        server.setAllowCompression(true);
+//        server.setMaxRetries(1); // defaults to 0.  > 1 not recommended.
         return server;
     }
 }
